1c3782bbd7fe3538a6be067e9eadd7d4f52c9d87
Filipe Reis <110914641+devfilipemarques@users.noreply.github.com>
2023-03-17 17:16:07 -0300

    version 1
    Subindo a primeira versão do Projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormInputComponent } from './form-input/form-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
